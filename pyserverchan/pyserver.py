2b5e84867f9e365630c4b1dd13b4270de8549993
GanjinZero <mike29@126.com>
2019-01-16 21:39:58 +0800

    Can upload 7KB picture

--- pyserverchan/pyserver.py
+++ pyserverchan/pyserver.py
@@ -5,6 +5,7 @@
 
 import requests
 import base64
+import os
 
 def check_is_web(string):
     check_list = ['www', 'com', 'net']
@@ -25,13 +26,16 @@ class ServerChan(object):
         # picture: A network address or a local address
         # may only support png
         # local picture cannot be sent now
+        # max size 7725byte?
         if check_is_web(picture):
             content = '![' + title + '](' + picture + ')'
         else:
             with open(picture, 'rb') as pic:
                 base64pic = base64.b64encode(pic.read())
-                content = '![' + title + '](data:image/png;base64,' + str(base64pic) + ')'
-        requests.get(self.url, params={'text':title, 'desp':content})
+                base64str = str(base64pic)[2:-1]
+                content = '![' + title + '][link1]' + os.linesep + os.linesep
+                content += '[link1]:data:image/png;base64,' + base64str
+        #requests.get(self.url, params={'text':title, 'desp':content})
         return
 
     def output_to_weixin_markdown(self, markdown_file, title='MarkdownFile'):
@@ -40,3 +44,6 @@ class ServerChan(object):
             content = md.read()
         requests.get(self.url, params={'text':title, 'desp':content})
         return
+
+if __name__ == "__main__":
+    svc.output_to_weixin_picture("C:\\Users\\GanjinZero\\Pictures\\saber.png")
